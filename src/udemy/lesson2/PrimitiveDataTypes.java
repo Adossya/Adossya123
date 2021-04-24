c2a6c569950ecbad46a099942b87609eadaee171
Adossya <adossya21@gmail.com>
2021-04-24 12:35:52 +0300

    test file

---
+++ src/udemy/lesson2/PrimitiveDataTypes.java
@@ -0,0 +1,55 @@
+package udemy.lesson2;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+        byte b3 = 127;
+
+        short s1 = 5;
+        short s2 = -10;
+        short s3 = 0;
+
+        int i1 = 500;
+
+        long l1 = 1000;
+        long l2 = 10000000000L;
+
+        float f1 = 3.14f;
+        float f3 = 20.0f;
+
+        double d1 = 5.5;
+        double d2 = 87.65;
+
+        char c1 = 'a';
+        char c2 = 'A';
+        char c3 = '7';
+        char c4 = ' ';
+
+        char c5 = 500; // 10 система исчесления
+
+        char c6 = '\u05AB'; // 16 система исчесления
+
+        boolean booll1 = true;
+        boolean booll2 = false;
+
+        int a1 = 60;
+
+        int a2 = 0b111100;
+
+        int a3 = 0B111100;
+
+        int a4 = 0_74;
+
+        int a5 = 0x3C;
+        int a6 = 0X3C;
+
+
+        int a7 = 1_000_000_000;
+
+        System.out.println(a7);
+
+
+    }
+}
---
+++ src/udemy/lesson2/homework2.java
@@ -0,0 +1,87 @@
+package udemy.lesson2;
+
+public class homework2 {
+    public static void main(String[] args) {
+        byte b1 = 12;
+        byte b2 = 0b1100;
+        byte b3 = 0_14;
+        byte b4 = 0xC;
+
+        short s1 = 1300;
+        short s2 = 0b10100010100;
+        short s3 = 0_2424;
+        short s4 = 0x514;
+
+        int i1 = 0;
+        int i2 = 0b0;
+        int i3 = 0_0;
+        int i4 = 0x0;
+
+        long l1 = 123456789L;
+        long l2 = 0b111010110111100110100010101L;
+        long l3 = 0_726746425L;
+        long l4 = 0x75BCD15L;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+        System.out.println();
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+        System.out.println();
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+        System.out.println();
+
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+        System.out.println();
+
+        float f1 = 3.14F;
+        float f2 = 2.5752F;
+
+        double d1 = 3.01;
+        double d2 = 1.01;
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println();
+
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println();
+
+        System.out.println(bool1);
+        System.out.println(bool2);
+        System.out.println();
+
+        char a = 'A';
+        char b = 'a';
+        char c = ' ';
+        char d = '1';
+        char e = 100;
+        char f = '\u0064';
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+    }
+}
