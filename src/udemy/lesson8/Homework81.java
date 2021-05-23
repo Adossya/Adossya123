6b2dcf39dccd598d04ffc98e6975e4e624c3bfc9
Adossya <adossya21@gmail.com>
2021-05-23 17:43:41 +0300

    test file

--- src/udemy/lesson8/Homework81.java
+++ src/udemy/lesson8/Homework81.java
@@ -1,26 +1,31 @@
 package udemy.lesson8;
 
 public class Homework81 {
-    static final double Pi=3.14;
-    double Ploshad (double a, double b){
-        double S=Pi*a*b;
+    public static final double Pi=3.14;
+
+    public double Ploshad (double radius){
+        double S=Pi*radius*radius;
         /*System.out.println("Ploshad ravna: "+S);*/
         return S;
         }
-    static double Dlinna_okrugnosti (double a){
-        double dlinna=2*Pi*a;
+
+    static double Dlinna_okrugnosti (double radius2){
+        double dlinna=2*Pi*radius2;
         /*System.out.println("Dlinna okrugnosti ravna: "+dlinna);*/
         return dlinna;
         }
-    void showInfo (double d){
-        System.out.println("Radius: "+d+" Kruga ploshad ravna: "+Ploshad(3,4)+" Dlinna okrugnosti: "+Dlinna_okrugnosti(3));
+
+    void showInfo (double radius3){
+        System.out.println("Radius: "+radius3);
+        System.out.println("Kruga ploshad ravna: "+Ploshad(radius3));
+        System.out.println("Dlinna okrugnosti: "+Dlinna_okrugnosti(radius3));
     }
 }
 class Homework82 {
     public static void main(String[] args) {
-        Homework81.Dlinna_okrugnosti(3);
         Homework81 h1=new Homework81();
-        h1.Ploshad(5,4);
-        h1.showInfo(9);
+        h1.Ploshad(5);
+        Homework81.Dlinna_okrugnosti(3);
+        h1.showInfo(4);
     }
 }
