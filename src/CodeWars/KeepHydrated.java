b79f9279c98cbb62fcfb97381258a2cdf453f335
Adossya <adossya21@gmail.com>
2021-06-04 22:24:38 +0300

    test file

--- src/CodeWars/KeepHydrated.java
+++ src/CodeWars/KeepHydrated.java
@@ -10,9 +10,14 @@ public class KeepHydrated {
         return a;
 
     }
+    public int Liters2(double time){
+
+        return time>0? (int)Math.floor(time * 0.5):0;
+    }
 
     public static void main(String[] args) {
         KeepHydrated r1=new KeepHydrated();
         System.out.println(r1.Liters(11.8));
+        System.out.println(r1.Liters2(11.8));
     }
 }
