2212187409ef6fcee8f93d0afc864769bf49753a
Adossya <adossya21@gmail.com>
2021-06-07 18:39:02 +0300

    test file

--- src/CodeWars/OppositesAttract1.java
+++ src/CodeWars/OppositesAttract1.java
@@ -17,7 +17,7 @@ public class OppositesAttract1 {
         boolean a=flower1!=flower2? true :(flower1%2==0 && flower2%2==1) || (flower1%2==1 && flower2%2==0)? true:false;
         return a && b;
     }
-    //varinat 3 (podsmotrel uge v reshennih solutions
+    //varinat 3 (podsmotrel uge v reshennih solution
     public static boolean isLove3(final int flower1, final int flower2){
         return flower1%2 != flower2%2;  //flower1%2 == flower2%2 ?false:true;
     }
