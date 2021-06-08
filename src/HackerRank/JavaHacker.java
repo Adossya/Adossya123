7762b1b06ab9c57119e4c10a52d5a769eb13d34b
Adossya <adossya21@gmail.com>
2021-06-08 23:06:48 +0300

    test file

---
+++ src/HackerRank/JavaHacker.java
@@ -0,0 +1,13 @@
+package HackerRank;
+
+import java.util.Scanner;
+
+public class JavaHacker {
+    public static void main(String[] args) {
+        Scanner s1=new Scanner(System.in);
+        int n=s1.nextInt();
+        if (n>=-100 && n<=100){
+            System.out.println("Good job");
+        } else {System.out.println("Wrong answer");}
+    }
+}
