63adb77d1d58a96a75d435c288bab93cfced8632
Adossya <adossya21@gmail.com>
2021-06-10 22:02:29 +0300

    test file

--- src/CodeWars/TwiceAsOld.java
+++ src/CodeWars/TwiceAsOld.java
@@ -2,5 +2,17 @@ package CodeWars;
 
 public class TwiceAsOld {
 
+    public static int TwiceAsOld(int dadYears, int sonYears){
+        int old=dadYears-sonYears;
+        int razn=old*2;
+        int otv=razn-dadYears;
 
+
+        return otv>0?otv:-otv;}
+}
+
+
+    public static void main(String[] args) {
+        System.out.println(TwiceAsOld(36,7));
+    }
 }
