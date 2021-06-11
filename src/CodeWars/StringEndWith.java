83fd8a287ae0a9abc1d929de5f4ca1bed982174b
Adossya <adossya21@gmail.com>
2021-06-12 00:20:51 +0300

    test file

---
+++ src/CodeWars/StringEndWith.java
@@ -0,0 +1,12 @@
+package CodeWars;
+
+public class StringEndWith {
+
+    public static boolean solution(String str, String ending) {
+
+        return str.endsWith(ending);
+    }
+    public static void main(String[] args) {
+        System.out.println(solution("samurai","ra"));
+    }
+}
