777e3e2bca7e8907b56bb1c6c08862c1424c17a0
Adossya <adossya21@gmail.com>
2021-06-24 19:33:13 +0300

    test file

---
+++ src/CodeWars/Println.java
@@ -0,0 +1,7 @@
+package CodeWars;
+
+public class Println {
+    public static void main(String[] args) {
+        System.out.println("Сегодня иду пить пиво, а на гитхаб надо что-то запостить,простите меня");
+    }
+}
