ae7ecbaac298273ccda6fb460cd33fcdc1565165
Adossya <adossya21@gmail.com>
2021-06-25 20:19:51 +0300

    test file

---
+++ src/CodeWars/printlnDenDva.java
@@ -0,0 +1,7 @@
+package CodeWars;
+
+public class printlnDenDva {
+    public static void main(String[] args) {
+        System.out.println("Сегодня даже впадло было мейн писать, иду чуток выпить, снова=)");
+    }
+}
