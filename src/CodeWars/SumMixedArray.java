50c747c24fc7d63e0185cd8a433711b49f1f2a1d
Adossya <adossya21@gmail.com>
2021-06-28 22:50:16 +0300

    test file

---
+++ src/CodeWars/SumMixedArray.java
@@ -0,0 +1,19 @@
+package CodeWars;
+//
+//
+//import java.util.Arrays;
+//import java.util.List;
+//
+//public class SumMixedArray {
+//    public int sum(List<?> mixed) {
+//        int suma=0;
+//        for (int a:mixed){
+//            suma+=a;
+//        }
+//        return suma;
+//    }
+//
+//    public static void main(String[] args) {
+//        sum(Arrays.asList(5,5));
+//    }
+//}
