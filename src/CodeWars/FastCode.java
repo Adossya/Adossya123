81e11d8432669e647f1b53c1d0829aebdfe33fe3
Adossya <adossya21@gmail.com>
2021-07-14 23:01:34 +0300

    test file

---
+++ src/CodeWars/FastCode.java
@@ -0,0 +1,14 @@
+package CodeWars;
+
+public class FastCode {
+
+        public static int doubleInteger(int i) {
+            // Double the integer and return it!
+            return i*2;
+        }
+
+
+    public static void main(String[] args) {
+        System.out.println(doubleInteger(45));
+    }
+}
