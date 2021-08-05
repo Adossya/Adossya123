6697fa4fa8053af7d2e6068034f3a0b967690622
Adossya <adossya21@gmail.com>
2021-08-05 23:10:32 +0300

    test file

---
+++ src/udemy/lesson30EqualsToStringWrapperClasses/Test2.java
@@ -0,0 +1,8 @@
+package udemy.lesson30EqualsToStringWrapperClasses;
+import java.util.ArrayList;
+public class Test2 {
+    public static void main(String[] args) {
+        ArrayList <Integer>list=new ArrayList<Integer>();
+        list.add(5);
+    }
+}
