059f25a9a9d182f2349f34e4ef3a52e57d753646
Adossya <adossya21@gmail.com>
2021-08-06 23:28:26 +0300

    test file

---
+++ src/udemy/lesson30EqualsToStringWrapperClasses/test3.java
@@ -0,0 +1,12 @@
+package udemy.lesson30EqualsToStringWrapperClasses;
+
+public class test3 {
+    {
+        System.out.println("Hello");
+    }
+
+
+    public static void main(String[] args) {
+        test3 t=new test3();
+    }
+}
---
+++ src/udemy/lesson30EqualsToStringWrapperClasses/test4.java
@@ -0,0 +1,7 @@
+package udemy.lesson30EqualsToStringWrapperClasses;
+
+public class test4 {
+    public static void main(String[] args) {
+        System.out.println("Dosluhal leckiu. a tam net dz");
+    }
+}
