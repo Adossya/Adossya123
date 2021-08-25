6769bef973d800ccba6f12a8c52f7a88cbe413dd
Adossya <adossya21@gmail.com>
2021-08-25 22:59:30 +0300

    test file

---
+++ src/udemy/lesson31ErrorsPart1/Test9.java
@@ -0,0 +1,36 @@
+package udemy.lesson31ErrorsPart1;
+
+import java.io.*;
+
+public class Test9 {
+    void abc () throws FileNotFoundException{
+        try {
+            File f = new File("text10.txt");
+            FileInputStream fis = new FileInputStream(f);
+        } catch (FileNotFoundException e) {
+            System.out.println("Exception poyman i nemnogo obrabotan");
+            throw e;
+        } finally {
+            System.out.println("eto block finally");
+        }
+    }
+    void method  () {
+        try {
+        abc();}
+        catch (FileNotFoundException e){
+            System.out.println("Polnost'u obrabotan");
+        }
+    }
+
+//    void def () {
+//        try {
+//            int [] array={1,2,3};
+//            System.out.println(array[5]);
+//        } catch (ArrayIndexOutOfBoundsException e) {
+//            System.out.println("Exception poyman i nemnogo obrabotan");
+//            throw e;
+//        } finally {
+//            System.out.println("eto block finally");
+//        }
+//    }
+}
