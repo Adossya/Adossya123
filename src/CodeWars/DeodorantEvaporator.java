f1c3d1c234a11e34da7061408c6fc7f3bb8779e8
Adossya <adossya21@gmail.com>
2021-08-27 00:02:01 +0300

    test file

---
+++ src/CodeWars/DeodorantEvaporator.java
@@ -0,0 +1,18 @@
+package CodeWars;
+
+public class DeodorantEvaporator {
+    public static int evaporator(double content, double evap_per_day, double threshold) {
+        threshold=content/100*threshold;
+        int days=0;
+        for (content=content; content>threshold;){
+            content=content-(content/100*evap_per_day);
+            days+=1;
+        }
+
+        return days;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(evaporator(10, 10, 5));
+    }
+}
