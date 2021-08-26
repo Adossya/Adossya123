69e4e7e3372f27bd3496f7ac5cd87865177bfa6a
Adossya <adossya21@gmail.com>
2021-08-27 00:07:22 +0300

    test file

--- src/CodeWars/Evaporator.java
+++ src/CodeWars/Evaporator.java
@@ -4,11 +4,9 @@ public class Evaporator {
     public static int evaporator(double content, double evap_per_day, double threshold) {
         threshold=content/100*threshold;
         int days=0;
-        for (content=content; content>threshold;){
+        for (content=content; content>threshold; days++){
             content=content-(content/100*evap_per_day);
-            days+=1;
         }
-
         return days;
     }
 
