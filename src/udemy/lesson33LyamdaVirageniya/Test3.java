45dedb5478e909d94a4b9e0fd0f6ffbb329a2204
Adossya <adossya21@gmail.com>
2021-09-02 23:50:02 +0300

    test file

--- src/udemy/lesson33LyamdaVirageniya/Test3.java
+++ src/udemy/lesson33LyamdaVirageniya/Test3.java
@@ -2,3 +2,18 @@ package udemy.lesson33LyamdaVirageniya;
 
 public class Test3 {
 }
+class Student {}
+//    String name;
+//    char sex;
+//    int age;
+//    int course;
+//    double avgGrade;
+//
+//    Student(String name, char sex, int age, int course, double avgGrade) {
+//        this.name = name;
+//        this.sex = sex;
+//        this.age = age;
+//        this.course = course;
+//        this.avgGrade = avgGrade;
+//    }
+//}
