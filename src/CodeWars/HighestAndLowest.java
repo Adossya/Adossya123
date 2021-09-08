7543887685befc1d38bb043ae2ab0de37ecb760a
Adossya <adossya21@gmail.com>
2021-09-08 23:51:51 +0300

    test file

---
+++ src/CodeWars/HighestAndLowest.java
@@ -0,0 +1,13 @@
+package CodeWars;
+
+public class HighestAndLowest {
+    public static String highAndLow(String numbers) {
+
+        return "throw towel";
+    }
+
+    public static void main(String[] args) {
+        String s="8 3 -5 42 -1 0 0 -9 4 7 4 -4";
+        highAndLow(s);
+    }
+}
