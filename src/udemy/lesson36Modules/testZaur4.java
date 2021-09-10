cecbeca151fa20f5ebc3ee232209a219077ed315
Adossya <adossya21@gmail.com>
2021-09-10 23:27:20 +0300

    test file

---
+++ src/udemy/lesson36Modules/testZaur4.java
@@ -0,0 +1,17 @@
+package udemy.lesson36Modules;
+
+import udemy.p1.p2.p3.p4.D;
+
+public class testZaur4 {
+    public static void main(String[] args) {
+        Integer i=new Integer(5);
+        Long l=new Long(10);
+        Double d=new Double(5);
+        System.out.println(i+l+d);
+//        System.out.println(i==l);
+//        System.out.println(i.equals(l));
+//
+//        System.out.println(i==d);
+//        System.out.println(i.equals(d));
+    }
+}
