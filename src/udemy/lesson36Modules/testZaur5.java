59cb364c76e00df325ea88eb2be51c8d2987bb93
Adossya <adossya21@gmail.com>
2021-09-11 23:47:11 +0300

    test file

---
+++ src/udemy/lesson36Modules/testZaur5.java
@@ -0,0 +1,9 @@
+package udemy.lesson36Modules;
+
+public class testZaur5 {
+    public static void main(String[] args) {
+        int []array1={-3,2,0,8,1};
+        int []arrray2={9,0,4,-10};
+      //  System.out.println(array1[(array1)=arrray2)[1]);
+    }
+}
