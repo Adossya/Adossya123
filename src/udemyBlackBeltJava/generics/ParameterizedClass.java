1ea943b35c2ad5479e2e187de546d77ea601d6f4
Adossya <adossya21@gmail.com>
2021-09-20 22:33:36 +0300

    test file

---
+++ src/udemyBlackBeltJava/generics/ParameterizedClass.java
@@ -0,0 +1,19 @@
+package udemyBlackBeltJava.generics;
+
+public class ParameterizedClass {
+    public static void main(String[] args) {
+        Info <String> info1=new Info<>("privet");
+        System.out.println(info1);
+    }
+}
+
+class Info <T> {
+    private T value;
+    public Info (T value){
+        this.value=value;
+    }
+    @Override
+    public String toString() {
+        return "{{" + value + "}}";
+    }
+}
