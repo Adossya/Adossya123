82a07f512c3488f2443c056ce323be7a5b3243fe
Adossya <adossya21@gmail.com>
2021-10-12 22:49:13 +0300

    test file

---
+++ src/Quizful/Test.java
@@ -0,0 +1,9 @@
+package Quizful;
+
+public class Test {
+    public static void main(String[] args) {
+        for (final int i:new int[] {1,2,3}){
+            System.out.println(i+1);
+        }
+    }
+}
--- src/udemyBlackBeltJava/Collection/ArrayListMethods1.java
+++ src/udemyBlackBeltJava/Collection/ArrayListMethods1.java
@@ -1,26 +1,31 @@
 package udemyBlackBeltJava.Collection;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ArrayListMethods1 {
+//public class ArrayListMethods1 {
+//    public static void main(String[] args) {
+//        List<String> arrayList1=new ArrayList<>();
+//        arrayList1.add("String");
+//        arrayList1.add("Strin23213g");
+//
+//            Integer i1=new Integer("1");
+//            System.out.println(i1);
+//            boolean b1;
+//            String s1="Zero";
+//            String s2="Zero";
+//            if (s1.equals(s2)){
+//                b1=true;
+//            }else {b1=false;}
+//        System.out.println(b1);
+//        }
+//    }
+class Main {
     public static void main(String[] args) {
-        ArrayList <String>arrayList1=new ArrayList<>();
-        arrayList1.add("String");
-        arrayList1.add("Strin23213g");
-
-        arrayList1.add("Strin3213123g");
-        arrayList1.add(0,"0000");
-
-        arrayList1.set(1,"Set");
-
-        for (String s:arrayList1){
-            System.out.print(s + " ");
+        int[] array1 = {1, 2};
+        for (long x : array1) {
+            System.out.println(x);
         }
-
-        System.out.println(arrayList1.get(0));
-        arrayList1.remove(0);
-        System.out.println(arrayList1);
-
     }
-
 }
+
