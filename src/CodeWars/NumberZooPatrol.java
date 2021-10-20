0fad03758556885162fd6fc55da65772a0b53efb
Adossya <adossya21@gmail.com>
2021-10-21 00:23:18 +0300

    test file

--- src/CodeWars/NumberZooPatrol.java
+++ src/CodeWars/NumberZooPatrol.java
@@ -7,15 +7,21 @@ import java.util.stream.Collectors;
 
 public class NumberZooPatrol {
     public static int findMissingNumber(int[] numbers) {
-
-
+        if (numbers.length==1){
+            return 2;
+        }
         List<Integer> list1 = Arrays.stream(numbers).boxed().collect(Collectors.toList());
+        if (list1.isEmpty()){
+            return 1;
+        }
         Collections.sort(list1);
 
         int[] array = list1.stream().mapToInt(Integer::intValue).toArray();
-
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 1; j < array.length; j++) {
+                if (array.length == 0) {
+                    return 1;
+                }
                 if (array[0] != 1) {
                     return 1;
                 }
@@ -23,13 +29,9 @@ public class NumberZooPatrol {
                     return array[j] - 1;
                 }
             }
-
         }
-        return 0;
-
+        return 5;
     }
-
-
     public static void main(String[] args) {
         System.out.println(findMissingNumber(new int[]{1}));
     }
