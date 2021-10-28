db1e1d6157fdd04f55c3c789360b23405cac4b2e
Adossya <adossya21@gmail.com>
2021-10-28 22:51:05 +0300

    test file

--- src/udemyBlackBeltJava/Arraylistsss/Arraylistmethods.java
+++ src/udemyBlackBeltJava/Arraylistsss/Arraylistmethods.java
@@ -1,13 +1,28 @@
 package udemyBlackBeltJava.Arraylistsss;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Arraylistmethods {
     public static void main(String[] args) {
+        ArrayList<String> arrayList1=new ArrayList<>();
+        arrayList1.add("Zaur");
+        arrayList1.add("Ivan");
+        arrayList1.add("Mariya");
+//        System.out.println("Arraylist: "+arrayList1);
+//        Object [] array=arrayList1.toArray();
+//        String [] array5=arrayList1.toArray(new String[0]);
+//        for (String s:array5) {
+//            System.out.println(s + " ");
+//        }
+//
+        List<Integer> list1=List.of(3,8,13);
+        System.out.println(list1);
 
-        int then=0;
-        System.out.println(then);
-
-
+        List<String> list2=List.copyOf(arrayList1);
+        System.out.println(list2);
+//        list1.add(100);
 
     }
 }
---
+++ src/udemyBlackBeltJava/iterator/iteratorExample.java
@@ -0,0 +1,20 @@
+package udemyBlackBeltJava.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class iteratorExample {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList1=new ArrayList<>();
+        arrayList1.add("Zaur");
+        arrayList1.add("Ivan");
+        arrayList1.add("Mariya");
+
+        Iterator <String> iterator1=arrayList1.iterator();
+        while (iterator1.hasNext()){
+            iterator1.next();
+
+            iterator1.remove();
+        }
+    }
+}
