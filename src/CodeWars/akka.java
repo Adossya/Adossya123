af208c9ed99634ef3bf01d89f0768783bd539852
Adossya <adossya21@gmail.com>
2021-11-01 21:33:04 +0200

    test file

--- src/CodeWars/akka.java
+++ src/CodeWars/akka.java
@@ -4,5 +4,6 @@ public class akka {
     public static void main(String[] args) {
         System.out.println("akka");
         System.out.println("maka");
+        System.out.println("maka");
     }
 }
