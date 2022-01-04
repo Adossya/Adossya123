e529804ca87e395da208ab79cd119cf52a209a87
Adossya <adossya21@gmail.com>
2022-01-04 23:12:36 +0200

    test file

--- src/udemyBlackBeltJava/lambda/StudentInfo.java
+++ src/udemyBlackBeltJava/lambda/StudentInfo.java
@@ -2,6 +2,16 @@ package udemyBlackBeltJava.lambda;
 
 import java.util.ArrayList;
 
+
+class StudentInfooo {
+    void testStudent (ArrayList<Student> al, StudentChecks sc){
+        for (Student s:al){
+            if (sc.check(s)){
+                System.out.println(s);
+            }
+        }
+    }
+}
 public class StudentInfo {
     public static void main(String[] args) {
         Student st1=new Student("Ivan",'m',22,6,8.3);
@@ -15,9 +25,58 @@ public class StudentInfo {
         students.add(st3);
         students.add(st4);
         students.add(st5);
+        StudentInfooo info=new StudentInfooo();
+        info.testStudent (students,new checkOvergrade());
+        System.out.println("-----");
+//        info.testStudent(students, new StudentChecks() {
+//            @Override
+//            public boolean check(Student s) {
+//                return s.age<30;
+//            }
+//        });
+
+        info.testStudent(students,(Student s) -> {return s.age<30;});
+
+        System.out.println("======");
+        info.testStudent(students,(Student s)->{return s.avr_Grade>8;} );
+        System.out.println("-----");
+        info.testStudent(students,(Student s)->{
+            return s.age>20 && s.avr_Grade<10 && s.sex=='m';
+        });
+//        info.printStudentsOverGrade(students,8);
+//        System.out.println("++++++++");
+//        info.printStudentsUnderrGrade(students,20);
+//        System.out.println("++++++++");
+//        info.printStudentsMixCondition(students,17,7,'f');
 
     }
-    void printStudentsOverGrade (ArrayList <Student> al, double avr_Grade){}
-    void printStudentsUnderrGrade (ArrayList <Student> al, int age){}
-    void printStudentsOGrade (ArrayList <Student> al, double avr_Grade){}
+//    void printStudentsOverGrade (ArrayList <Student> al, double avr_Grade){
+//        for (Student s:al){
+//            if (s.avr_Grade>avr_Grade){
+//            System.out.println(s);}
+//        }
+//    }
+//    void printStudentsUnderrGrade (ArrayList <Student> al, int age){
+//        for (Student s:al){
+//            if (s.age<age){
+//                System.out.println(s);}
+//        }
+//    }
+//    void printStudentsMixCondition (ArrayList <Student> al, int age, double grade, char sex){
+//        for (Student s:al){
+//            if (s.age>age && s.avr_Grade<grade && s.sex==sex){
+//                System.out.println(s);}
+//        }
+//    }
+}
+
+interface StudentChecks {
+    boolean check(Student s);
+}
+
+class checkOvergrade implements StudentChecks {
+    @Override
+    public boolean check(Student s) {
+        return s.avr_Grade>8;
+    }
 }
