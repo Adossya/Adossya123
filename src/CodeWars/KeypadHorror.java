417c7de832e261f4e30df6c43036754994302e63
Adossya <adossya21@gmail.com>
2022-01-11 01:03:05 +0200

    test file

---
+++ src/CodeWars/KeypadHorror.java
@@ -0,0 +1,38 @@
+package CodeWars;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KeypadHorror {
+    public static void main(String[] args) {
+        computerToPhone("123");
+    }
+    public static String computerToPhone(String number){
+        char [] array1=number.toCharArray();
+        char [] array2=new char[number.length()];
+
+        for (int i=0;i<array1.length;i++){
+
+            switch (array1[i]){
+
+                case '1': array2[i]='7'; break;
+                case '2': array2[i]='8'; break;
+                case '3': array2[i]='9'; break;
+                case '4': array2[i]='4'; break;
+                case '5': array2[i]='5'; break;
+                case '6': array2[i]='6'; break;
+                case '7': array2[i]='1'; break;
+                case '8': array2[i]='2'; break;
+                case '9': array2[i]='3'; break;
+                case '0': array2[i]='0'; break;
+                default:array2[i]=' '; break;
+            }
+
+        }
+        String s1=new String(array2);
+        return s1;
+
+    }
+}
