0c7c21dc04b2a504a7a34c0f5756a3c05bc2f9f0
Adossya <adossya21@gmail.com>
2022-02-19 22:58:45 +0200

    test file

--- src/udemyBlackBeltJava/Stream/Reduce.java
+++ src/udemyBlackBeltJava/Stream/Reduce.java
@@ -2,6 +2,7 @@ package udemyBlackBeltJava.Stream;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Reduce {
     public static void main(String[] args) {
@@ -9,8 +10,26 @@ public class Reduce {
         list.add(5);
         list.add(8);
         list.add(2);
-        list.add(4);
-        list.add(3);
-        int result=list.stream().reduce((accumulator, element)->accumulator*element).get();
-    }
-}
+        int result=list.stream().reduce((accumulator, element)->accumulator*element).get(); //accumulator
+
+        int result2=list.stream().reduce(2,(accumulator, element)->accumulator*element); //accumulator
+        System.out.println(result2);
+
+        List <String> list3=new ArrayList<>();
+        list3.add("privet");
+        list3.add("kak dela");
+        list3.add("ok");
+        list3.add("poka");
+        String result3=list3.stream().reduce((accumulator,e)->accumulator+""+e).get();
+        System.out.println(result3);
+
+//        List<Integer> list100=new ArrayList<>();
+//        Optional<Integer> o =list.stream().reduce((accumulator, element)->accumulator*element);
+//        if (o.isPresent()){
+//            System.out.println(o.get());
+//        } else {
+//            System.out.println("Not Present");
+//        }
+
+//        System.out.println(result);
+}}
--- src/udemyBlackBeltJava/Stream/filter.java
+++ src/udemyBlackBeltJava/Stream/filter.java
@@ -18,8 +18,10 @@ public class filter {
         students.add(st4);
         students.add(st5);
 
-        students= students.stream().filter(element
-                ->element.getAge()>22 && element.getAvgGrade()<7.2).collect(Collectors.toList());
+//        students= students.stream().filter(element
+//                ->element.getAge()>22 && element.getAvgGrade()<7.2).collect(Collectors.toList());
+        System.out.println(students);
+        students= students.stream().sorted((x,y)->x.getName().compareTo(y.getName())).collect(Collectors.toList());
         System.out.println(students);
     }
 
