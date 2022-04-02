24afae3b1b2bd17f327fe8dcda47c0df87f2f459
Adossya <adossya21@gmail.com>
2022-04-02 23:11:02 +0300

    test file

---
+++ src/CodeWars/NonConsecutiveNumber.java
@@ -0,0 +1,17 @@
+package CodeWars;
+
+public class NonConsecutiveNumber {
+    static Integer find(final int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] - array[i - 1] != 1) {
+                return array[i];
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println(find(array));
+    }
+}
